d4c05e6701d050542f04bc06bdb99724a0761501
Ross Jennings <rossjjennings@gmail.com>
2017-11-06 14:07:12 -0500

    Added command-line parsing

--- calc.cpp
+++ calc.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "parse.h"
 
-int main()
+int main(int argc, char *argv[])
 {
     std::string expr;
     
-    std::cout << "Enter an expression:" << std::endl;
-    std::getline(std::cin, expr);
-    std::cout << "You entered: " << expr << std::endl;
+    if(argc == 1)
+    {
+        std::cout << "Enter an expression:" << std::endl;
+        std::getline(std::cin, expr);
+    }
+    else
+    {
+        std::vector<std::string> args(argv + 1, argv + argc);
+        for(std::string arg : args)
+        {
+            expr += arg;
+            expr += ' ';
+        }
+    }
     
+    std::cout << "You entered: " << expr << std::endl;
     auto syntax_tree = parse(expr);
     std::cout << "This evaluates to " << syntax_tree->evaluate() << "." << std::endl;
     return 0;
